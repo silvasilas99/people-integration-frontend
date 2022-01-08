e5533e014e0124cf36a17f6fdc8000f346fbedd3
Silas Santos da Silva <silvasilas66@gmail.com>
2022-01-07 22:40:38 -0300

    Basic routing to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Views/Login/Login';
+import GoogleCallback from './Views/GoogleCallback/GoogleCallback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/auth/google/callback" element={<GoogleCallback />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
